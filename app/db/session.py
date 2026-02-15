7f4a729d784a8c06d1ca1f484b0d643281545e4c
jet locati <jetlocati4922@gmail.com>
2026-02-15 02:28:00 -0700

    resolve PSQL @ root 5427

--- app/db/session.py
+++ app/db/session.py
@@ -2,5 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+#settings on how to spicicly connect to DB
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+
+#makes temporary sessions to connect to docker for moments in time. then closes
+#time is specified in the .env file as ACCESS_TOKEN_EXPIRE_MIN, but can be changed here as well.
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
